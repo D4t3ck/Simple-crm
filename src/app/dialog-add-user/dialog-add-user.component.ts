40115f35d1226af189268541489e2c77f2e83fd5
daniel sinteck <d.sint3ck@gmail.com>
2024-06-20 23:40:55 +0200

    ++ form fields;

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions],
+  imports: [
+    MatDialogContent,
+    MatDialogActions,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {
-
-
-}
+export class DialogAddUserComponent {}
