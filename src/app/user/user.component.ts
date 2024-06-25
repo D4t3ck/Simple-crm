76aa2272adf6540135aea715e6aa1189349571f3
daniel sinteck <d.sint3ck@gmail.com>
2024-06-25 19:29:58 +0200

    ++ user table link to DB;

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -6,11 +6,15 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
+    CommonModule,
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
@@ -18,12 +22,27 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule,
   ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss',
+  styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: User = new User();
+  allUsers: any[] = [];
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
 
-  constructor(public dialog: MatDialog) {}
+
+  constructor(public dialog: MatDialog) {
+    const aCollection = collection(this.firestore, 'items');
+    this.items$ = collectionData(aCollection);
+  }
+
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    collectionData(usersCollection).subscribe((changes: any) => {
+      console.log('Received changes from DB', changes);
+      this.allUsers = changes;
+    });
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
