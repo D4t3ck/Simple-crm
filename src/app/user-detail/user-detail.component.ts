18cdaf2cebcf5ba123abe3c13907072e2a69e600
daniel sinteck <d.sint3ck@gmail.com>
2024-06-28 14:27:41 +0200

    ++ paramMap for id;

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
+  userId: string | null = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap) => {
+      this.userId = paramMap.get('id');
+      console.log('received current ID', this.userId);
+      
+    });
+  }
 }
